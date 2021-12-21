d1de5a0a32791acd7eaa1130b3ae0d23740c1d2a
blondi2003 <natash.erschowa2016@yandex.ru>
2021-12-21 14:29:51 +0400

    Добавьте файлы проекта.

---
+++ laaaab.7.2/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string stringNumbers = "92 33 52 86 75 12 54 96 82 65 56 20 87";
+
+        string[] numbers = stringNumbers.Split(' ');
+
+        var evenNumbers = numbers.Where(n => Convert.ToInt32(n) % 2 == 0);
+
+        Console.WriteLine(evenNumbers.Count());
+
+
+        Console.WriteLine();
+
+
+        char[] charNumbers = stringNumbers.ToCharArray();
+
+        int countEvenNumbers = 0;
+
+        string tempNumber = "";
+
+        foreach (char c in charNumbers)
+        {
+            if (c == ' ')
+            {
+                if (Convert.ToInt32(tempNumber) % 2 == 0)
+                {
+                    countEvenNumbers++;
+                }
+                tempNumber = "";
+            }
+            else
+            {
+                tempNumber += c;
+            }
+        }
+
+        Console.WriteLine(countEvenNumbers);
+    }
+}
---
+++ laaab.7/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string text = "Hello world!";
+
+        var charTextLinq = text.Select(x => (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') ? '+' : x);
+
+        foreach (char c in charTextLinq)
+        {
+            Console.Write(c);
+        }
+
+        Console.WriteLine();
+
+        char[] charText = text.ToCharArray();
+
+        for (int i = 0; i < charText.Length; i++)
+        {
+            if ((charText[i] >= 'a' && charText[i] <= 'z') || (charText[i] >= 'A' && charText[i] <= 'Z'))
+            {
+                charText[i] = '+';
+            }
+        }
+
+        foreach (char c in charText)
+        {
+            Console.Write(c);
+        }
+    }
+}
